cb6b9e17c9cbcd67c1de977e78836d4f7c5ad2e6
Overstars <1635396212@qq.com>
2023-03-07 17:08:48 +0800

    [update]看代码并写注释

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
+/*
+ * 创建一个 Redux store 来以存放应用中所有的 state。
+ * 应用中应有且仅有一个 store。
+ */
 
 const store = createStore(rootReducer, window.devToolsExtension && window.devToolsExtension());
 
